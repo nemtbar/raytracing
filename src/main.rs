5bc3a27669942538e4a445fc526b9cf0c68d7b3c
nemtbar <nemethbarnabas51@gmail.com>
2024-04-02 23:38:40 +0200

    object enum

--- src/main.rs
+++ src/main.rs
@@ -64,49 +64,66 @@ impl Ray {
     }
 }
 
-
-
-
-struct Sphere {
-    pos: Vec3,
-    col: Pixel,
-    rad: f32,
+pub struct HitInfo {
+    p: Point,
+    normal: Vec3,
+    color: Pixel
 }
 
-impl Sphere {
-    fn new(pos: Vec3, col: Pixel, rad: f32) -> Self{
-        Self {pos, col, rad}
-    }
+pub enum Object {
+    Sphere {pos: Vec3, col: Pixel, rad: f32}
 }
 
-
-fn intersect(ray: Ray, spheres: Vec<Sphere>) -> Option<Pixel> {
-    //https://kylehalladay.com/blog/tutorial/math/2013/12/24/Ray-Sphere-Intersection.html
-    let mut min_d: f32 = 100000.;
-    let mut color = None;
-    for sphere in spheres{
-        let l = &sphere.pos - &ray.start;
-        let tc = l.dot(&ray.dir);
-        if tc < 0.0{
-            continue;
-        } else {
-            let d = (l.length() * l.length())-(tc*tc);
-            let rad2 = sphere.rad * sphere.rad;
-            if d > rad2|| d > min_d{
-                continue;
-            } else {
-                let thc = (rad2 - d).sqrt();
-                let t0 = tc - thc;
-                let normal = (&ray.dir * t0 - &sphere.pos).normalize();
-                min_d = d;
-                color = Some(Pixel::new((normal.x * 255.) as u8, (normal.y * 255.) as u8, (normal.z * 255.) as u8));
+impl Object {
+    fn intersect(&self, ray: &Ray) -> Option<HitInfo>{
+        //https://kylehalladay.com/blog/tutorial/math/2013/12/24/Ray-Sphere-Intersection.html
+        match self {
+            Self::Sphere {pos, col, rad} => {
+                let l = pos - &ray.start;
+                let tc = l.dot(&ray.dir);
+                if tc < 0.0{
+                    None
+                } else {
+                    let d = (l.length() * l.length())-(tc*tc);
+                    let rad2 = rad * rad;
+                    if d > rad2{
+                        None
+                    } else {
+                        let thc = (rad2 - d).sqrt();
+                        let t0 = tc - thc;
+                        let normal = (&ray.dir * t0 - pos) / rad;
+                        //let color = Pixel::new((normal.x * 255.) as u8, (normal.y * 255.) as u8, (normal.z * 255.) as u8);
+                        Some(HitInfo{p: &ray.dir * t0, normal, color: *col})
+                    }
+                }
             }
         }
     }
-    color
 
 
+    pub fn hit_all(ray: &Ray, lis: Vec<Self>) -> Option<HitInfo>{
+        let mut inf: Option<HitInfo> = None;
+        let mut min_dist = 100000.;
+        for obj in lis {
+            match Self::intersect(&obj, ray){
+                Some(i) => {
+                    let len = (&i.p - &ray.start).length();
+                    if len < min_dist {
+                        inf = Some(i);
+                        min_dist = len;
+                    } else {
+                        continue;
+                    }
+                }
+
+                None => continue,
+            }
+        }
+
+        inf
+    }
 }
+
 
 fn frag(x: usize, y: usize) -> Pixel{
     //uv coordinates between -g->g
@@ -119,15 +136,15 @@ fn frag(x: usize, y: usize) -> Pixel{
     let mut dir = Vec3::new(ux, camera.y+1., uy)-&camera;
 
     //rotation
-    let angle = 180.;
+    let angle = 90.;
     camera = camera.rot_x(angle);
     dir = dir.rot_x(angle).normalize();
     let ray = Ray::new(camera, dir);
-    let spheres = vec![
-        Sphere::new(Vec3::new(0., 0., 0.), Pixel::new(255, 0, 0), 1.),
+    let objects: Vec<Object> = vec![
+        Object::Sphere {pos: Vec3::new(0., 0., 0.), col: Pixel::new(255, 0, 0), rad: 1.}
     ];
-    match intersect(ray, spheres) {
-        Some(color) => return color,
+    match Object::hit_all(&ray, objects) {
+        Some(i) => i.color,
         None => {
             //background color
             let value =(uy + g) / (2.* g);
