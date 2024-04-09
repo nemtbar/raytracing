fb58b8c3eaec2ebb6567909cf8b40fa89d275709
nemtbar <nemethbarnabas51@gmail.com>
2024-04-09 16:12:59 +0200

    hi

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod geometry;
 use vec3::{Vec3, Point};
 use render::{Pixel, Image};
 use geometry::{Object, Ray};
-use std::env;
 
 //https://raytracing.github.io/books/RayTracingInOneWeekend.html
 
@@ -17,27 +16,25 @@ fn frag(x: usize, y: usize, width: usize, height: usize) -> Pixel{
     let ux = (x as f32) / (width as f32) * (g  * 2.)- g;
     let uy = ((y as f32) / (height as f32) * (g * 2.) - g) * -1.;
     //define important points
-    let camera: Point = Vec3::new(0., -5., 0.);
-    let dir = (Vec3::new(ux, -4., uy)-&camera).normalize();
+    let mut camera: Point = Vec3::new(0., -5., 0.);
+    let mut dir = (Vec3::new(ux, camera.y+1., uy)-&camera).normalize();
+    //camera = camera.rot_x(-20.);
+    //dir = dir.rot_x(-20.);
     let ray = Ray::new(camera.clone(), dir.clone());
     let objects: Vec<Object> = vec![
-        Object::Sphere {pos: Vec3::new(-3., 1., 0.), col: Vec3::new(1., 1., 1.), rad: 1., emmision: 0.},
-        Object::Plane { pos: Vec3::new(0., 0., -1.5), normal: Vec3::new(0., 0., 1.), col: Vec3::new(1., 1., 1.)},
+        Object::Sphere {pos: Vec3::new(-3., 1., 0.7), col: Vec3::new(1., 1., 1.), rad: 1., emmision: 0.},
+        Object::Plane {pos: Vec3::new(0., 0., -0.5), normal: Vec3::new(0., 0., 1.), col: Vec3::new(1., 1., 1.)},
         //Object::Sphere {pos: Vec3::new(0., 0., -40.), col: Vec3::new(1., 1., 1.), rad: 39., emmision: 0.},
-        Object::Sphere {pos: Vec3::new(0., 0., 1.), col: Vec3::new(1., 1., 1.), rad: 2., emmision: 0.3}
+        Object::Sphere {pos: Vec3::new(0., 0., 1.7), col: Vec3::new(1., 1., 1.), rad: 2., emmision: 0.3}
     ];
     let mut col = Pixel::default();
-    let mut atr: [u8; 2] = [20, 30];
-    let arguments = env::args().collect::<Vec<String>>();
-    for i in 1..arguments.len().min(atr.len()+1){
-        let num = arguments[i].parse::<u8>().expect("cannot convert argument to valid number");
-        atr[i-1] = num;
-    }
-    for i in 0..atr[0]{
+    let c = 10;
+    let max_b = 30;
+    for i in 0..c{
         if i == 0{
-            col = Object::bounce(ray.clone(), &objects, atr[1]);
+            col = Object::bounce(ray.clone(), &objects, max_b);
         } else {
-            col = Object::bounce(ray.clone(), &objects, atr[1]).lerp(&col, 1.- 1./(atr[0] as f32));
+            col = Object::bounce(ray.clone(), &objects, max_b).lerp(&col, 1.- 1./(c as f32));
         }
     }
     col
