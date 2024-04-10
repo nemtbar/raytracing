7911e3a1a86cb4ce43978bc24b8e82732202bbf1
nemtbar <nemethbarnabas51@gmail.com>
2024-04-10 16:29:49 +0200

    just

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,12 @@ fn frag(x: usize, y: usize, width: usize, height: usize) -> Pixel{
     let g = 1.;
     let ux = (x as f32) / (width as f32) * (g  * 2.)- g;
     let uy = ((y as f32) / (height as f32) * (g * 2.) - g) * -1.;
-    //define important points
     let camera: Point = Vec3::new(0., -5., 0.);
     let objects: Vec<Object> = vec![
         Object::Sphere {pos: Vec3::new(-3., 1., 0.), col: Vec3::new(1., 1., 1.), rad: 1.},
-        Object::Plane { pos: Vec3::new(0., 0., -1.5), normal: Vec3::new(1., 1., -1.), col: Vec3::new(1., 1., 1.)},
+        Object::Plane { pos: Vec3::new(0., 0., -1.5), normal: Vec3::new(0., 0., -1.), col: Vec3::new(1., 1., 1.)},
         //Object::Sphere {pos: Vec3::new(0., 0., -40.), col: Vec3::new(1., 1., 1.), rad: 39.},
-        Object::Sphere {pos: Vec3::new(0., 0., 1.2), col: Vec3::new(1., 1., 1.), rad: 2.}
+        Object::Sphere {pos: Vec3::new(0., 0., 1.), col: Vec3::new(1., 1., 1.), rad: 2.}
     ];
     let mut col = Pixel::default();
     let c = 10;
